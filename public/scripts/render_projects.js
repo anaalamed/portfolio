1bffbf807409138510919e312670eacb83ede37d
Ana <ana@Anas-MacBook-Air.local>
2021-08-21 16:31:51 +0300

    screenshots/render projects

--- public/scripts/render_projects.js
+++ public/scripts/render_projects.js
@@ -4,14 +4,14 @@ const projects = [
         "description": 'Social media website, where you can create your own profile, share posts, comment to posts, "like" posts and view other users and their profile and their posts.',
         "link": "https://anaalamed-forum-ui.herokuapp.com",
         "image": "images/forum.png",
-        "tools": ['react', 'node-js'],
+        "tools": ['react', 'node-js', 'sass'],
     },
     {
         "name": "To Do", 
         "description": `Website where you can create your own account and a "to-do" list. You can mark certain things off your list as "done", which will then be shown on a second list as things you've accomplished. Two more features of this website enables you to delete things you wrote or update them.`,
         "link": "https://anaalamed-todo.herokuapp.com/",
         "image": "images/todo.png",
-        "tools": ["react", 'node-js'],
+        "tools": ["react", 'node-js', 'sass'],
     },
     {
         "name": "Advertisement", 
@@ -72,12 +72,12 @@ function render_project(project = {}) {
             <div class="details details-ftm">
                 <h3>${project.name}</h3>
                 <div>
-                    ${project.tools?.includes('react') ? `<i class="fab fa-react fa-2x"></i>` : ``}
-                    ${project.tools?.includes('html') ? `<i class="fab fa-html5 fa-2x"></i>` : ``}
-                    ${project.tools?.includes('css') ? `<i class="fab fa-css3 fa-2x"></i>` : ``}
-                    ${project.tools?.includes('sass') ? `<i class="fab fa-sass fa-2x"></i>` : ``}
-                    ${project.tools?.includes('js') ? `<i class="fab fa-js fa-2x"></i>` : ``}
-                    ${project.tools?.includes('node-js') ? `<i class="fab fa-node-js fa-2x"></i>` : ``}                
+                ${project.tools?.includes('node-js') ? `<i class="fab fa-node-js fa-2x"></i>` : ``}                
+                ${project.tools?.includes('react') ? `<i class="fab fa-react fa-2x"></i>` : ``}
+                ${project.tools?.includes('html') ? `<i class="fab fa-html5 fa-2x"></i>` : ``}
+                ${project.tools?.includes('css') ? `<i class="fab fa-css3 fa-2x"></i>` : ``}
+                ${project.tools?.includes('sass') ? `<i class="fab fa-sass fa-2x"></i>` : ``}
+                ${project.tools?.includes('js') ? `<i class="fab fa-js fa-2x"></i>` : ``}
                 </div>
                 <p>${project.description}</p>
 
@@ -96,12 +96,12 @@ function render_project(project = {}) {
             <div class="details details-ftm">
                 <h3>${project.name}</h3>
                 <div>
+                ${project.tools?.includes('node-js') ? `<i class="fab fa-node-js fa-2x"></i>` : ``}                
                 ${project.tools?.includes('react') ? `<i class="fab fa-react fa-2x"></i>` : ``}
                 ${project.tools?.includes('html') ? `<i class="fab fa-html5 fa-2x"></i>` : ``}
                 ${project.tools?.includes('css') ? `<i class="fab fa-css3 fa-2x"></i>` : ``}
                 ${project.tools?.includes('sass') ? `<i class="fab fa-sass fa-2x"></i>` : ``}
-                ${project.tools?.includes('js') ? `<i class="fab fa-js fa-2x"></i>` : ``}
-                ${project.tools?.includes('node-js') ? `<i class="fab fa-node-js fa-2x"></i>` : ``}                
+                ${project.tools?.includes('js') ? `<i class="fab fa-js fa-2x"></i>` : ``}             
             </div>
                 <p>${project.description}</p>
                 <a class="my-button" target="_blank" href=${project.link}>Visit
