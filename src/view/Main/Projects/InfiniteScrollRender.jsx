9932514477d075a474de6dbce90d7fce0bcbe5fa
Ana Levit <anaalamed@gmail.com>
2024-02-09 12:22:23 +0200

    change project "updated at" to be pushed_at from github API

--- src/view/Main/Projects/InfiniteScrollRender.jsx
+++ src/view/Main/Projects/InfiniteScrollRender.jsx
@@ -7,32 +7,32 @@ import styled from "styled-components";
 import Project from './ProjectCard';
 
 const InfiniteScrollRender = ({ repos }) => {
-    const [currentRepos, setCurrentRepos] = useState(repos.slice(0, 6));
-    const [hasMore, setHasMore] = useState(true);
+  const [currentRepos, setCurrentRepos] = useState(repos.slice(0, 6));
+  const [hasMore, setHasMore] = useState(true);
 
-    const fetchMoreData = () => {
-        if (repos.length === 0) {
-            setHasMore(false);
-        }
+  const fetchMoreData = () => {
+    if (repos.length === 0) {
+      setHasMore(false);
+    }
 
-        setTimeout(() => {
-            setCurrentRepos(currentRepos.concat(repos.slice(0, 6)))
-            repos.splice(0, 6);
-        }, 1500);
-    };
+    setTimeout(() => {
+      setCurrentRepos(currentRepos.concat(repos.slice(0, 6)))
+      repos.splice(0, 6);
+    }, 1500);
+  };
 
-    return (
-        <Box
-            dataLength={currentRepos.length}
-            next={fetchMoreData}
-            hasMore={hasMore}
-            loader={<h4 style={{ textAlign: 'center' }}>Loading...</h4>}
-        >
-            {currentRepos?.map(repo =>
-                (<Project id={repo.id} name={repo.name} image_url={repo.image_url} description={repo.description} html_url={repo.html_url} updated_at={repo.updated_at} homepage={repo.homepage} topics={repo.topics}  ></Project>) // key={repo.id} ts error
-            )}
-        </Box>
-    );
+  return (
+    <Box
+      dataLength={currentRepos.length}
+      next={fetchMoreData}
+      hasMore={hasMore}
+      loader={<h4 style={{ textAlign: 'center' }}>Loading...</h4>}
+    >
+      {currentRepos?.map(repo =>
+        (<Project id={repo.id} name={repo.name} image_url={repo.image_url} description={repo.description} html_url={repo.html_url} pushed_at={repo.pushed_at} homepage={repo.homepage} topics={repo.topics}  ></Project>) // key={repo.id} ts error
+      )}
+    </Box>
+  );
 };
 export default InfiniteScrollRender;
 
--- src/view/Main/Projects/Pagination.tsx
+++ src/view/Main/Projects/Pagination.tsx
@@ -1,57 +1,76 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Pagination } from 'antd';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import 'antd/dist/antd.css';
+import { Pagination } from "antd";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import "antd/dist/antd.css";
 
-import Project from './ProjectCard';
+import Project from "./ProjectCard";
 import { devices } from "../../../styles/responsive";
 
 interface Repos {
-    [repo: number]: object
+  [repo: number]: object;
 }
 
 const PaginatonRender: React.FC<Repos> = ({ repos }) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [reposPerPage] = useState(6);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reposPerPage] = useState(6);
 
-    // sort projects
-    if (repos.length !== 0) {
-        repos.sort(function (a, b) {
-            var pr1 = a.order || 100;
-            var pr2 = b.order || 100;
-            return pr1 - pr2;
-        });
-    }
+  // sort projects
+  if (repos.length !== 0) {
+    repos.sort(function (a, b) {
+      var pr1 = a.order || 100;
+      var pr2 = b.order || 100;
+      return pr1 - pr2;
+    });
+  }
 
-    // slice for pages
-    const indexOfLastRepo = currentPage * reposPerPage;
-    const indexOfFirstRepo = indexOfLastRepo - reposPerPage;
-    var currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
+  // slice for pages
+  const indexOfLastRepo = currentPage * reposPerPage;
+  const indexOfFirstRepo = indexOfLastRepo - reposPerPage;
+  var currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
 
-    const handleChangePage = pageNumber => setCurrentPage(pageNumber);
+  const handleChangePage = (pageNumber) => setCurrentPage(pageNumber);
 
+  return (
+    <>
+      <AnchorLink href="#projects">
+        <Pagination
+          current={currentPage}
+          onChange={handleChangePage}
+          total={repos?.length || 0}
+          pageSize={reposPerPage}
+        />
+      </AnchorLink>
 
-    return (
-        <>
-            <AnchorLink href='#projects' >
-                <Pagination current={currentPage} onChange={handleChangePage} total={repos?.length || 0} pageSize={reposPerPage} />
-            </AnchorLink>
+      <Collection>
+        {currentRepos?.map(
+          (repo) => (
+            <Project
+              id={repo.id}
+              name={repo.name}
+              image_url={repo.image_url}
+              description={repo.description}
+              html_url={repo.html_url}
+              pushed_at={repo.pushed_at}
+              homepage={repo.homepage}
+              topics={repo.topics}
+            ></Project>
+          ) // key={repo.id} ts error
+        )}
+      </Collection>
 
-            <Collection>
-                {currentRepos?.map(repo =>
-                    (<Project id={repo.id} name={repo.name} image_url={repo.image_url} description={repo.description} html_url={repo.html_url} updated_at={repo.updated_at} homepage={repo.homepage} topics={repo.topics} ></Project>) // key={repo.id} ts error
-                )}
-            </Collection>
-
-            <AnchorLink href='#projects' >
-                <Pagination current={currentPage} onChange={handleChangePage} total={repos?.length || 0} pageSize={reposPerPage} />
-            </AnchorLink>
-        </>
-    );
+      <AnchorLink href="#projects">
+        <Pagination
+          current={currentPage}
+          onChange={handleChangePage}
+          total={repos?.length || 0}
+          pageSize={reposPerPage}
+        />
+      </AnchorLink>
+    </>
+  );
 };
 export default PaginatonRender;
-
 
 const Collection = styled.div`
   display: grid;
